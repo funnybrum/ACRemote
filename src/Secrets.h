fa164bc7d9f6518a8176b292ec50a31525c5321b
funnybrum <funnybrum@gmail.com>
2017-12-05 06:30:11 +0200

    Adding --assume-unchanged instruction to Secrets.h

--- src/Secrets.h
+++ src/Secrets.h
@@ -1,6 +1,8 @@
 #ifndef Secrets_h
 #define Secrets_h
 
+// Execut 'git update-index --assume-unchanged ./src/Secrets.h' to avoid tracking changes to this file.
+
 const char* WIFI_SSID = "wifi_network_ssid";
 const char* WIFI_PASSWORD = "wifi_network_password";
 
