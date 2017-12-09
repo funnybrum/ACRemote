90f71f2197a5e0fd7905371de6af9d7991db3176
funnybrum <funnybrum@gmail.com>
2017-12-10 00:00:52 +0200

    Fixing a compilation error.

--- lib/WebServer/WebServer.h
+++ lib/WebServer/WebServer.h
@@ -1,12 +1,19 @@
 #ifndef _WEBSERVER_H_
 #define _WEBSERVER_H_
 
+#include <ESP8266WebServer.h>
+
 class WebServer {
     public:
         void begin();
         void loop();
     private:
-        
+      ESP8266WebServer *_server;
+      
+      void handle_root();
+      void handle_on();
+      void handle_off();
 };
+
 
 #endif
